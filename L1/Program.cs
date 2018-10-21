4b73b7180c5c1b3f41a5a37ff01f9f8fea927723
Degen <lars-degen1@web.de>
2018-10-21 17:11:58 +0200

    Lesson 1

---
+++ L1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace L01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int argsAsInt = Int32.Parse(args[0]);
+               Console.WriteLine(RomanConversion.GetRomanNumber(argsAsInt));
+               
+           }
+
+        }
+    }
