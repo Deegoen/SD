c4aed76eff0cfd0aef8dee73d78c3e8f4c7886f1
Degen <lars-degen1@web.de>
2018-11-07 10:51:08 +0100

    L04 + L4.1

---
+++ L4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+public class SimplePerson
+{
+    public string FirstName;
+    public string LastName;
+    public DateTime DateOfBirth;
+}
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person root = Familytree.BuildTree();
+
+            Person found = Familytree.Find(root);
+            WriteLine(root);
+            WriteLine(found.FirstName + " " + found.LastName + " " + found.DateOfBirth);
+            
+            
+            //WriteLine(found.Dad.FirstName + " " + found.Dad.LastName);
+            //WriteLine(found.Mom.FirstName + " " + found.Mom.LastName);
+        }
+    }
+}
---
+++ L4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace L4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] test = new string[] {"Hallo", "Softwaredesigner"};
+            
+            for (int i = 0; i < test.Length; i++)
+                Console.WriteLine(test[i]);
+        }
+    }
+}
