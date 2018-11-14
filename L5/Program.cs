21011442c8a3cea61709ebd2dff9a9f2c5969d84
Degen <lars-degen1@web.de>
2018-11-14 10:07:14 +0100

    L5

---
+++ L5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace L5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter String:");
+            string toReverseString = Console.ReadLine();
+
+            Console.WriteLine("Original String:" + " " + toReverseString);
+            Console.WriteLine("Reversed String:" + " " + ReverseString(toReverseString));
+            //Console.WriteLine("Reversed Words:" + " " + ReverseWords(toReverseString));
+            //Console.WriteLine("Reversed Every Word:" + " " + ReverseEveryWord(toReverseString));
+        }
+        private static string ReverseString(string toReverseString)
+        {
+            string reversedString = "";
+
+            for (int i = toReverseString.Length - 1; i >= 0; i--)
+            {
+                reversedString += toReverseString[i];
+            }
+
+            return reversedString;
+        }
+
+        private static string ReverseWords(string toReverseString)
+        {
+            string test = "";
+
+            return test;
+        }
+
+        private static string ReverseEveryWord(string toReverseString)
+        {
+            string test = "";
+
+            return test;
+        }
+    }
+}
