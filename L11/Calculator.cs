2ab55d4cf3771c1e088ea76cddfe3a26808d5fff
Degen <lars-degen1@web.de>
2019-01-13 16:16:27 +0100

    L11

---
+++ L11/Calculator.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace L11
+{
+    delegate void ReportProgressMethod(int progress);
+    class Calculator
+    {
+        public event ReportProgressMethod ProgressMethod;
+        public Calculator(){
+            ProgressMethod += ConsoleOutputProgressInPercent;
+            ProgressMethod += ConsoleOutputProgressInfo;
+        }
+        public void CalculateSomething(){
+            for(int i = 0; i <= 100; i++){
+               if(i == 20 || i == 40 || i == 60 || i == 80 || i == 100){
+                   ProgressMethod(i);
+               }
+            }
+        }
+        public void ConsoleOutputProgressInPercent(int _progress){
+            Console.WriteLine(_progress + " %");
+        }
+        public void ConsoleOutputProgressInfo(int _progress){
+            if(_progress == 100){
+                Console.WriteLine("Done");
+            }else{
+                Console.WriteLine("Loading...");
+            }
+        }
+    }
+}
---
+++ L11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace L11
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.CalculateSomething();
+        }
+    }
+}
